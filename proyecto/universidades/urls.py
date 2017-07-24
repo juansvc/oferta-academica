4800b2325c92173090c9f13d82749890a1dde780
Juan Sebastián Vivanco <jsvivanco2@utpl.edu.ec>
2017-07-23 20:32:59 -0500

    Página universidades añadida, buscador, mapa de matriz, sede y extension

--- proyecto/universidades/urls.py
+++ proyecto/universidades/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('universidades.views',
     url(r'^index', 'index', name="index"),
     url(r'^estadisticas', 'estadisticas', name="estadisticas"),
+    url(r'^universidades', 'universidades', name="universidades"),
+    url(r'^universidad', 'universidad', name="universidad"),
     url(r'^$', 'index',name="index"),
 )
--- proyecto/universidades/views.py
+++ proyecto/universidades/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, InvalidPage
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext, loader
 from django.http import HttpResponse
@@ -5,7 +7,54 @@ from .models import *
 
 # Create your views here.
 def index(request):
-	return render_to_response('index.html', context=RequestContext(request))
+	universidades_lista = Ies.objects.all()
+	cantidad_universidades = len(universidades_lista)
+	carreras_lista = Carreras.objects.all()
+	cantidad_carreras = len(carreras_lista)
+	areas_lista = Areas.objects.all()
+	cantidad_areas = len(areas_lista)
+	universidades_lista = Ies.objects.all()
+	cantidad_universidades = len(universidades_lista)
+	return render(request, 'index.html', {'cantidad_universidades':cantidad_universidades, 'cantidad_carreras':cantidad_carreras, 'cantidad_areas':cantidad_areas})
 
 def estadisticas(request):
-	return render_to_response('estadisticas.html', context=RequestContext(request))
+	area = Areas.objects.all()
+	return render(request, 'estadisticas.html', {'area':area})
+
+def universidades(request):
+	if request.GET.get('buscar_universidad') is not None:
+		nombre_universidad = request.GET.get('buscar_universidad')
+		universidades_lista = Ies.objects.filter(nombre__icontains=nombre_universidad)
+	else:
+		universidades_lista = Ies.objects.all()
+
+	paginator = Paginator(universidades_lista, 12) #U por pagina
+	page = request.GET.get('page')
+	try:
+		universidades = paginator.page(page)
+	except PageNotAnInteger:
+		universidades = paginator.page(1)
+	except EmptyPage:
+		universidades = paginator.page(paginator.num_pages)
+
+	buscar_universidad = request.GET.get('buscar_universidad')
+	cantidad_universidades = len(universidades_lista)
+
+	return render(request, 'universidades.html', {'universidades':universidades, 'buscar_universidad':buscar_universidad, 'cantidad_universidades':cantidad_universidades})
+
+def universidad(request):
+	id = request.GET.get('id')
+	institucion = Ies.objects.get(pk=id)
+	matriz = Matriz.objects.get(id_ies=id)
+	listado_carreras = IesCarreras.objects.filter(id_ies=id)
+	try:
+		sede = Sede.objects.get(id_ies=id)
+		extension = Extension.objects.get(id_ies=id)
+		return render(request, 'universidad.html', {'institucion':institucion, 'matriz':matriz, 'listado_carreras':listado_carreras, 'sede':sede, 'extension':extension})
+	except ObjectDoesNotExist:
+		sede = 0
+		extension = 0
+		return render(request, 'universidad.html', {'institucion':institucion, 'matriz':matriz, 'listado_carreras':listado_carreras})
+
+
+	
